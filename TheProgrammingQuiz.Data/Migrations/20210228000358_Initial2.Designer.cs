c7e5b1ed19380e14ac28a934815cfe5c609e88cf
Volen <volen1999@gmail.com>
2021-07-06 19:40:12 +0300

    Add .sln

--- TheProgrammingQuiz.Data/Migrations/20210228000358_Initial2.Designer.cs
+++ TheProgrammingQuiz.Data/Migrations/20210228000358_Initial2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TheProgrammingQuiz.Data;
 
 namespace TheProgrammingQuiz.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210228000358_Initial2")]
+    partial class Initial2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
