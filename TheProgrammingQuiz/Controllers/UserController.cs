81593420b7a122df5410ce94a3fd95197b0b3b90
Volen <volen1999@gmail.com>
2021-08-23 10:39:33 +0300

    Fix namespaces

--- TheProgrammingQuiz/Controllers/UserController.cs
+++ TheProgrammingQuiz/Controllers/UserController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+
 using TheProgrammingQuiz.Data;
 using TheProgrammingQuiz.Data.Models;
 
